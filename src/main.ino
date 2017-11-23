8303861f27a69dd9bee7faaeb782b42d7e0b3792
paulsvsp@umich.edu <paulization@gmail.com>
2017-11-23 13:33:44 -0500

    Setting up

---
+++ src/main.ino
@@ -0,0 +1,24 @@
+// Libraries
+// LCD
+
+// INPUT pins
+// Gyroscope
+// Start button?
+// Proximity sensor
+
+// OUTPUT pins
+// LCD
+// Speaker
+
+// Variables
+
+
+// States
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+
+}
